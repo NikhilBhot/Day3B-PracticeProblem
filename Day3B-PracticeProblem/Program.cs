834d1158398abf18d9fc68c4a9cf32d5a751bc96
unknown <bhotnikhil25@gmail.com>
2023-06-26 00:19:51 +0530

    [Nikhil]-Type Of Constructor Example

---
+++ Day3B-PracticeProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day3B_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Of Constructor");
+            TypeOfConstrcutorExample.DemoMethod();
+        }
+    }
+}
---
+++ Day3B-PracticeProblem/TypeOfConstrcutorExample.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_PracticeProblem
+{
+    //Demonstrate different types of constructors.
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        // Default Constructor
+        public Person()
+        {
+            name = "Unknown";
+            age = 0;
+        }
+
+        // Parameterized Constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Copy Constructor
+        public Person(Person other)
+        {
+            this.name = other.name;
+            this.age = other.age;
+        }
+
+        // Static Constructor
+        static Person()
+        {
+            Console.WriteLine("Static Constructor called.");
+        }
+
+        // Instance Method
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+        }
+    }
+
+    public class TypeOfConstrcutorExample
+    {
+       public static void DemoMethod()
+        {
+            // Default Constructor
+            Person person1 = new Person();
+            Console.WriteLine("Default Constructor:");
+            person1.DisplayInfo();
+            Console.WriteLine();
+
+            // Parameterized Constructor
+            Person person2 = new Person("John", 25);
+            Console.WriteLine("Parameterized Constructor:");
+            person2.DisplayInfo();
+            Console.WriteLine();
+
+            // Copy Constructor
+            Person person3 = new Person(person2);
+            Console.WriteLine("Copy Constructor:");
+            person3.DisplayInfo();
+        }
+    }
+}
